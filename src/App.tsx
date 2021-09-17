b7b5fe202a5cc1bd6b1815db76f8ed2bd58b4fcf
Brad W <thatbr4d@gmail.com>
2021-09-17 08:25:22 -0400

    add react bootstrap

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
+import { Carousel, Col, Row } from 'react-bootstrap';
+import Container from 'react-bootstrap/Container';
 
 function App() {
   return (
-    <div></div>
+    <Container fluid>
+      <Row className="justify-content-md-center">
+        <Col lg="auto">
+          <h1 className="header">React Lunch and Learn</h1>
+        </Col>
+      </Row>
+    </Container>
   );
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
+// Import the Bootstrap CSS
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
